63b28d6622c4b7ea55a8a1baf3a0acb1e346be16
ElenaSergeevnaKhot <121289862+ElenaSergeevnaKhot@users.noreply.github.com>
2024-02-26 22:53:05 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,25 +3,26 @@ function solveEquation(a, b, c) {
   let arr = [];
   (root1, root2) = (-b ± sq(b2-4ac))/2
   
-  let r1, r2;
-  let a = prompt("Enter the first number: ");
-  let b = prompt("Enter the second number: ");
-  let c = prompt("Enter the third number: ");
-  
-  let d = b * b - 4 * a * c;
-  
-  if (d > 0) { r1 = (-b + Math.sqrt(d) )/(2*a); r2 = -b - Math.sqrt(d) )/(2*a);
-  console.log(`The roots of quadratic equation are ${r1} and ${r2}`);
-  
-  }
-  else if (d == 0) { r1 = r2 = -b / (2 * a);
-  console.log(`The roots of quadratic equation are ${r1} and ${r2}`);
-  else 
-  console.log(`The Roots are Complex and Different`);
-  
-  return arr;
+let r1, r2;
+
+let a = prompt("Enter the first number: ");
+let b = prompt("Enter the second number: ");
+let c = prompt("Enter the third number: ");
+
+
+let D = b * b - 4 * a * c;
+
+
+if (D > 0) {
+    r1 = (-b + Math.sqrt(D)) / (2 * a);    r2 = (-b - Math.sqrt(D)) / (2 * a);
+    console.log(`The roots of quadratic equation are ${r1} and ${r2}`);
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+else if (D == 0) {
+    r1 = r2 = -b / (2 * a);
+    console.log(`The roots of quadratic equation are ${r1} and ${r2}`);
 }
+
+else 
+    console.log(`The Roots are Complex and Different `);
+
