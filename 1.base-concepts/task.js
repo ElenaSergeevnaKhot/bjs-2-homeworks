e63ff25fbe6158279869f9bb193495140d3c5848
ElenaSergeevnaKhot <121289862+ElenaSergeevnaKhot@users.noreply.github.com>
2024-02-28 23:21:41 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,28 +1,13 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  (root1, root2) = (-b ± sq(b2-4ac))/2
-  
-let r1, r2;
-
-let a = prompt("Enter the first number: ");
-let b = prompt("Enter the second number: ");
-let c = prompt("Enter the third number: ");
-
-
-let D = b * b - 4 * a * c;
-
-
-if (D > 0) {
-    r1 = (-b + Math.sqrt(D)) / (2 * a);    r2 = (-b - Math.sqrt(D)) / (2 * a);
-    console.log(`The roots of quadratic equation are ${r1} and ${r2}`);
+  let roots = [];
+  let D = b ** 2 - 4 * a * c;
+  if (D > 0) {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+  } else if (D === 0) {
+    let x = -b / (2 * a);
+  }
+  return roots;
 }
-
-else if (D == 0) {
-    r1 = r2 = -b / (2 * a);
-    console.log(`The roots of quadratic equation are ${r1} and ${r2}`);
-}
-
-else 
-    console.log(`The Roots are Complex and Different `);
-
