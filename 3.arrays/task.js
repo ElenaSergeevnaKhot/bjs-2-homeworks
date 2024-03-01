4beeb887b403b6409c5dc7cc9c8ec7437ac21dd0
ElenaSergeevnaKhot <121289862+ElenaSergeevnaKhot@users.noreply.github.com>
2024-03-01 12:36:42 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.lenght !== arr2.lenght) {
+    return false;
+  }
+  return arr1.every((value, index) => {
+    console.log(value, arr2[index]);
+    return value === arr2[index];
+  });
 }
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+console.log(compareArrays([8, 9], [6, 7])); // false, разные значения
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])); // false, разные значения
+console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])); // false, разные значения
+console.log(compareArrays([1, 2, 3], [2, 3, 1])); // false, разные индексы, хотя и одинаковые значения
+console.log(compareArrays([8, 1, 2], [8, 1, 2])); // true
