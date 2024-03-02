3933e309a76921ce4f17b185e64bc708df649560
ElenaSergeevnaKhot <121289862+ElenaSergeevnaKhot@users.noreply.github.com>
2024-03-02 20:46:54 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,11 +6,13 @@ function solveEquation(a, b, c) {
   let D = b ** 2 - 4 * a * c;
   if (D > 0) {
     let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    roots.push(x1);
     let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    roots.push(x2);
   } else if (D === 0) {
     let x = -b / (2 * a);
+    roots.push(x);
   }
-  roots.push(x1, x2);
-
+  
   return roots;
 }
